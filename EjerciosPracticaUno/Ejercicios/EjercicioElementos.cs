a59d2e7e50863a73a9e703b1937fe61a570b4a6b
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-09-07 17:17:59 -0500

    Parcial y ejercicios dinamicos todos mas Extra Recursividad

---
+++ EjerciosPracticaUno/Ejercicios/EjercicioElementos.cs
@@ -0,0 +1,60 @@
+﻿using EjerciosPracticaUno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciosPracticaUno.Ejercicios
+{
+    public class EjercicioElementos
+    {
+        Elemento elemento;
+        List<string> nombre;
+        List<int> valor;
+        int cantElementos;
+        int valAcumulado, cantValor20;
+
+        public EjercicioElementos()
+        {
+            elemento = new();
+            nombre = new();
+            valor = new();
+            cantElementos = new();
+            valAcumulado = new();
+            cantValor20 = new();
+        }
+
+        public void RegistrarElementos()
+        {
+            Console.WriteLine("Ingrese la cantidad de elementos a registrar:");
+            cantElementos = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cantElementos; i++)
+            {
+                Console.WriteLine("Nombre elemento: \n");
+                nombre.Add(Console.ReadLine());
+                Console.WriteLine("Valor: \n");
+                valor.Add(int.Parse(Console.ReadLine()));
+                valAcumulado += valor[i];
+            }
+            elemento.Nombres = nombre.ToArray();
+            elemento.Valores = valor.ToArray();
+            Console.WriteLine($"El valor acumulado es: {valAcumulado}");
+            valAcumulado = 0;
+            for (int i = 0; i < elemento.Valores.Length; i++)
+            {
+                
+                if (valor[i] >= 10)
+                    valAcumulado += valor[i];
+            }
+            Console.WriteLine($"El valor acumulado para mayores que 10 es: {valAcumulado}");
+            for (int i = 0; i < elemento.Valores.Length; i++)
+            {
+                valAcumulado = 0;
+                if (valor[i] >= 20)
+                    cantValor20++;
+            }
+            Console.WriteLine($"La cantidad de Valores mayor a 20 es: {cantValor20}");
+        }
+    }
+}
--- EjerciosPracticaUno/Program.cs
+++ EjerciosPracticaUno/Program.cs
@@ -11,17 +11,23 @@ namespace EjerciosPracticaUno
             EjercicioParcial ejercicioParcial = new();
             EjercicioPersona ejercicioPersona = new();
             EjercicioRecursividad ejercicioRecursividad = new();
+            EjercicioElementos ejercicioElementos = new();
             Console.WriteLine($"Escoge el ejercicio a realizar \n");
-            Console.WriteLine($"1) Lista edades Estudiante \n" +
+            Console.WriteLine($"1) Lista edades Estudiante\n" +
                 $"2) Promedio altura Personas\n" +
-                $"3) \n" +
+                $"3) Ejercicio Elementos\n" +
+                $"4) Lista Edades estudiantes (Dinamica)" +
                 $"5) Parcial\n" +
-                $"6) Recursividad\n");
+                $"6) Recursividad (Extra)\n");
             int opcion = int.Parse(Console.ReadLine());
             if (opcion == 1)
                 ejercicioEstudiante.RegistrarEdadEstudiantes();
             else if (opcion == 2)
                 ejercicioPersona.RegistrarPersona();
+            else if (opcion == 3)
+                ejercicioElementos.RegistrarElementos();
+            else if (opcion == 4)
+                ejercicioEstudiante.RegistrarEdadEstudiantes();
             else if (opcion == 5)
                 ejercicioParcial.RegistrarEmpleado();
             else if (opcion == 6)
@@ -30,7 +36,7 @@ namespace EjerciosPracticaUno
                 var num = int.Parse(Console.ReadLine());
                 ejercicioRecursividad.Calcular(num);
             }
-                
+
 
             else
                 Console.WriteLine("Ingresa una opcion Valida por favor. \n");
