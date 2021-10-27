1d48a5d343e916d6ad49da3f9ba846f9ba767fbb
Diego Alejandro Rios Vasquez <diego.rios@wasolutions.co>
2021-10-26 21:18:49 -0500

    Ejercicio Operaciones

---
+++ EjerciosPracticaUno/Ejercicios/EjercicioOperacionesMatematicas.cs
@@ -0,0 +1,40 @@
+﻿using EjerciosPracticaUno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciosPracticaUno.Ejercicios
+{
+    public class EjercicioOperacionesMatematicas
+    {
+        public static void Calcular(Operacion operacion)
+        {
+            Sumar(operacion);
+            Restar(operacion);
+            Multiplicar(operacion);
+            Dividir(operacion);
+        }
+
+        private static void Sumar(Operacion operacion)
+        {
+            Console.WriteLine($"El resultado de la Suma es : {operacion.X + operacion.Y}");
+        }
+
+        private static void Restar(Operacion operacion)
+        {
+            Console.WriteLine($"El resultado de la Resta es : {operacion.X - operacion.Y}");
+        }
+
+        private static void Multiplicar(Operacion operacion)
+        {
+            Console.WriteLine($"El resultado de la Multiplicacion es : {operacion.X * operacion.Y}");
+        }
+
+        private static void Dividir(Operacion operacion)
+        {
+            Console.WriteLine($"El resultado de la Division es : {operacion.X / operacion.Y}");
+        }
+    }
+}
--- EjerciosPracticaUno/Program.cs
+++ EjerciosPracticaUno/Program.cs
@@ -1,11 +1,12 @@
 ﻿using EjerciosPracticaUno.Ejercicios;
+using EjerciosPracticaUno.Models;
 using System;
 
 namespace EjerciosPracticaUno
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             EjercicioEstudiante ejercicioEstudiante = new();
             EjercicioParcial ejercicioParcial = new();
@@ -16,9 +17,10 @@ namespace EjerciosPracticaUno
             Console.WriteLine($"1) Lista edades Estudiante\n" +
                 $"2) Promedio altura Personas\n" +
                 $"3) Ejercicio Elementos\n" +
-                $"4) Lista Edades estudiantes (Dinamica)" +
+                $"4) Lista Edades estudiantes (Dinamica)\n" +
                 $"5) Parcial\n" +
-                $"6) Recursividad (Extra)\n");
+                $"6) Recursividad (Extra)\n" +
+                $"7) Operaciones Matematicas");
             int opcion = int.Parse(Console.ReadLine());
             if (opcion == 1)
                 ejercicioEstudiante.RegistrarEdadEstudiantes();
@@ -32,12 +34,23 @@ namespace EjerciosPracticaUno
                 ejercicioParcial.RegistrarEmpleado();
             else if (opcion == 6)
             {
-                Console.WriteLine("Ingrese el nuemero a dividir: ");
+                Console.WriteLine("Ingrese el numero a dividir: ");
                 var num = int.Parse(Console.ReadLine());
                 ejercicioRecursividad.Calcular(num);
+            }else if(opcion == 7)
+            {
+                Operacion operacion = new();
+                Console.WriteLine("A continuacion ingresa dos valores enteros X, Y y se calcularan las operaciones matematicas entre estos numeros");
+                Console.WriteLine("X = ");
+                operacion.X = int.Parse(Console.ReadLine());
+                Console.WriteLine("Y = ");
+                operacion.Y = int.Parse(Console.ReadLine());
+                EjercicioOperacionesMatematicas.Calcular(operacion);
+                Console.ReadKey();
             }
             else
                 Console.WriteLine("Ingresa una opcion Valida por favor. \n");
+            Main();
         }
     }
 }
